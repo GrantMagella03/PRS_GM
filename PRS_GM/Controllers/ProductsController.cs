9cdf6e3117733ea325852524a224f9ec3e1a7c83
Grant Magella <Bitman2003@gmail.com>
2023-08-31 15:46:10 -0400

    -reformating productsController

--- PRS_GM/Controllers/ProductsController.cs
+++ PRS_GM/Controllers/ProductsController.cs
@@ -1,49 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PRS_GM.Data;
 using PRS_GM.Models;
 
-namespace PRS_GM.Controllers
-{
+namespace PRS_GM.Controllers {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductsController : ControllerBase
-    {
+    public class ProductsController : ControllerBase {
         private readonly AppDbContext _context;
 
-        public ProductsController(AppDbContext context)
-        {
+        public ProductsController(AppDbContext context) {
             _context = context;
         }
 
         // GET: api/Products
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Product>>> GetProduct()
-        {
-          if (_context.Products == null)
-          {
-              return NotFound();
-          }
-            return await _context.Products.Include(x=>x.Vendor).ToListAsync();
+        public async Task<ActionResult<IEnumerable<Product>>> GetProduct() {
+            if (_context.Products == null) {
+                return NotFound();
+            }
+            return await _context.Products.Include(x => x.Vendor).ToListAsync();
         }
 
         // GET: api/Products/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
-        {
-          if (_context.Products == null)
-          {
-              return NotFound();
-          }
-            var product = await _context.Products.Include(x=>x.Vendor).Where(x=>x.VendorID==id).FirstOrDefaultAsync();
+        public async Task<ActionResult<Product>> GetProduct(int id) {
+            if (_context.Products == null) {
+                return NotFound();
+            }
+            var product = await _context.Products.Include(x => x.Vendor).Where(x => x.VendorID == id).FirstOrDefaultAsync();
 
-            if (product == null)
-            {
+            if (product == null) {
                 return NotFound();
             }
 
@@ -53,27 +40,20 @@ namespace PRS_GM.Controllers
         // PUT: api/Products/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutProduct(int id, Product product)
-        {
-            if (id != product.ID)
-            {
+        public async Task<IActionResult> PutProduct(int id, Product product) {
+            if (id != product.ID) {
                 return BadRequest();
             }
 
             _context.Entry(product).State = EntityState.Modified;
 
-            try
-            {
+            try {
                 await _context.SaveChangesAsync();
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ProductExists(id))
-                {
+            catch (DbUpdateConcurrencyException) {
+                if (!ProductExists(id)) {
                     return NotFound();
-                }
-                else
-                {
+                } else {
                     throw;
                 }
             }
@@ -84,12 +64,10 @@ namespace PRS_GM.Controllers
         // POST: api/Products
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Product>> PostProduct(Product product)
-        {
-          if (_context.Products == null)
-          {
-              return Problem("Entity set 'AppDbContext.Product'  is null.");
-          }
+        public async Task<ActionResult<Product>> PostProduct(Product product) {
+            if (_context.Products == null) {
+                return Problem("Entity set 'AppDbContext.Product'  is null.");
+            }
             _context.Products.Add(product);
             await _context.SaveChangesAsync();
 
@@ -98,15 +76,12 @@ namespace PRS_GM.Controllers
 
         // DELETE: api/Products/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteProduct(int id)
-        {
-            if (_context.Products == null)
-            {
+        public async Task<IActionResult> DeleteProduct(int id) {
+            if (_context.Products == null) {
                 return NotFound();
             }
             var product = await _context.Products.FindAsync(id);
-            if (product == null)
-            {
+            if (product == null) {
                 return NotFound();
             }
 
@@ -116,8 +91,7 @@ namespace PRS_GM.Controllers
             return NoContent();
         }
 
-        private bool ProductExists(int id)
-        {
+        private bool ProductExists(int id) {
             return (_context.Products?.Any(e => e.ID == id)).GetValueOrDefault();
         }
     }
