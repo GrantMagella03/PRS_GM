a770ea8e6ff5dd62ed37b467045ee8586a63d3c0
Grant Magella <Bitman2003@gmail.com>
2023-08-31 10:28:27 -0400

    Add project files.

---
+++ PRS_GM/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PRS_GM.Models;
+
+namespace PRS_GM.Data {
+    public class AppDbContext : DbContext {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) {
+        }
+        public DbSet<User> Users { get; set; } = default!;
+        public DbSet<Vendor> Vendors { get; set; } = default!;
+    }
+}
---
+++ PRS_GM/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace PRS_GM.Models {
+    [Index("Username", IsUnique = true)]
+    public class User {
+        [Key]public int ID { get; set; }
+        [StringLength(30)]public string Username { get; set; }
+        [StringLength(30)]public string Password { get; set; }
+        [StringLength (30)]public string Firstname { get; set; }
+        [StringLength(30)]public string Lastname { get; set; }
+        [StringLength(12)] public string? Phone { get; set; }
+        [StringLength(255)] public string? Email { get; set; }
+        public bool isReviewer { get; set; }
+        public bool isAdmin { get; set; }
+    }
+}
